604eee9195c9e218783671fcaecf64acaeab8fb9
manymengofishing <mattheyj@googlemail.com>
2013-11-04 20:31:35 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+  $(".ad-cover").click(function(){
+    $(this).toggleClass('closed');
+  });
+
+  var ua = navigator.userAgent,
+    isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
+
+  if (isMobileWebkit) {
+    $('html').addClass('mobile');
+  }
+
+  $(function(){
+    var iScrollInstance;
+
+    if (isMobileWebkit) {
+      iScrollInstance = new iScroll('wrapper');
+
+      $('#scroller').stellar({
+        scrollProperty: 'transform',
+        positionProperty: 'transform',
+        horizontalScrolling: false,
+        verticalOffset: 150
+      });
+    } else {
+      $.stellar({
+        horizontalScrolling: false,
+        verticalOffset: 150
+      });
+    }
+  });
+
+
+});
