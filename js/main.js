4adf54c45c61e0fa1b68c6a9cbc300457f43a329
manymengofishing <mattheyj@googlemail.com>
2013-11-05 12:38:33 -0500

    add function to onscroll event, 3 states scrolling over ad

--- js/main.js
+++ js/main.js
@@ -13,9 +13,28 @@ $(document).ready(function(){
 
   $(function(){
     var iScrollInstance;
+    var content1Bottom = $("#content1").position().top + $("#content1").height();
+    var originalAdHeight = $(".ad-cover").height();
 
     if (isMobileWebkit) {
-      iScrollInstance = new iScroll('wrapper');
+      iScrollInstance = new iScroll('wrapper',{
+        onScrollMove: function() {
+          var content1BottomOffset;
+          if((-iScrollInstance.y ) > content1Bottom){
+            content1BottomOffset = (-iScrollInstance.y) - content1Bottom;
+            $("#content2").prepend(content1BottomOffset);
+            $(".ad-cover").height(originalAdHeight+content1BottomOffset);
+          }
+
+
+          // 3 states
+
+          // expand down
+
+          // shrink up
+
+        }
+      });
 
       $('#scroller').stellar({
         scrollProperty: 'transform',
@@ -28,6 +47,15 @@ $(document).ready(function(){
         horizontalScrolling: false,
         verticalOffset: 150
       });
+    }
+  });
+
+  var adBottom = $(".ad-cover").position().top + $(".ad-cover").height();
+
+  $(document).scroll(function(){
+    var scrollTop = $(document).scrollTop();
+    if(scrollTop > adBottom){
+      $(".ad-cover").addClass('closed');
     }
   });
 
